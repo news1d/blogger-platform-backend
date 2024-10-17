b5bd966fd5ea054b34aac5d7591dbbe1f1ab6557
news1d <news1dee@yandex.ru>
2024-10-18 00:43:03 +0300

    Post API is completed

--- src/modules/posts/post-controller.ts
+++ src/modules/posts/post-controller.ts
@@ -1,20 +1,45 @@
+import {Request, Response} from 'express';
+import {postRepository} from "./post-repository";
+import {HTTP_STATUSES} from "../../http-statuses";
+import {PostInputModel, PostViewModel} from "../../types/post.type"
 
 
-// export const postController = {
-//     getPosts (req: Request, res: Response) {
-//         return
-//     },
-//     createPost (req: Request, res: Response) {
-//         return
-//     },
-//     getPostById (req: Request, res: Response) {
-//         return
-//     },
-//     updatePostById (req: Request, res: Response) {
-//         return
-//     },
-//     deletePostById (req: Request, res: Response) {
-//         return
-//     }
-//
-// }
+export const postController = {
+    getPosts (req: Request, res: Response<PostViewModel[]>) {
+        const posts = postRepository.getPosts();
+        res.status(HTTP_STATUSES.OK_200).json(posts);
+    },
+    createPost (req: Request<any, any, PostInputModel>, res: Response<PostViewModel>) {
+        const postId = postRepository.createPost(req.body);
+        const post = postRepository.getPostById(postId);
+
+        res.status(HTTP_STATUSES.CREATED_201).json(post);
+    },
+    getPostById (req: Request<{id: string}>, res: Response<PostViewModel>) {
+        const post = postRepository.getPostById(req.params.id);
+
+        if (post) {
+            res.status(HTTP_STATUSES.OK_200).json(post);
+        } else {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        }
+    },
+    updatePostById (req: Request<{id: string}, any, PostInputModel>, res: Response) {
+        const isUpdated = postRepository.updatePostById(req.params.id, req.body);
+
+        if (isUpdated) {
+            res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+        } else {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        }
+    },
+    deletePostById (req: Request, res: Response) {
+        const isDeleted = postRepository.deletePostById(req.params.id);
+
+        if (isDeleted) {
+            res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+        } else {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        }
+    }
+}
