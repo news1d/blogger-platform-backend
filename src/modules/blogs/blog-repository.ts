633428fc542c1f752963d6e10cbce3aa644eb186
news1d <news1dee@yandex.ru>
2024-11-07 21:34:09 +0300

    Migrated to Mongo Id and moved some functions to query repositories

--- src/modules/blogs/blog-repository.ts
+++ src/modules/blogs/blog-repository.ts
@@ -1,50 +1,14 @@
-import {BlogDBType, BlogInputModel, BlogViewModel} from "../../types/blog.types";
+import {BlogDBType, BlogInputModel} from "../../types/blog.types";
 import {blogCollection} from "../../db/mongoDb";
+import {ObjectId} from "mongodb";
 
 export const blogRepository = {
-    async getBlogs(pageNumber: number,
-                   pageSize: number,
-                   sortBy: string,
-                   sortDirection: 'asc' | 'desc',
-                   searchNameTerm: string | null): Promise<BlogViewModel[]> {
-
-        const filter: any = {}
-
-        if (searchNameTerm) {
-            filter.name = { $regex: searchNameTerm, $options: "i" };
-        }
-
-        const blogs = await blogCollection
-            .find(filter)
-            .sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
-            .skip((pageNumber - 1) * pageSize)
-            .limit(pageSize)
-            .toArray()
-
-        return blogs.map(this.mapToOutput)
-    },
-    async getBlogsCount(serchNameTerm: string | null): Promise<number> {
-        const filter: any = {}
-
-        if (serchNameTerm) {
-            filter.name = { $regex: serchNameTerm, $options: "i" };
-        }
-
-        return blogCollection.countDocuments(filter)
-    },
     async createBlog(blog: BlogDBType): Promise<string> {
-        await blogCollection.insertOne(blog);
-        return blog.id
-    },
-    async getBlogById(id: string): Promise<BlogViewModel | null>{
-        const blog = await blogCollection.findOne({id: id});
-        if (!blog) {
-            return null;
-        }
-        return this.mapToOutput(blog);
+        const result = await blogCollection.insertOne(blog)
+        return result.insertedId.toString();
     },
     async updateBlogById(id: string, body: BlogInputModel): Promise<boolean>{
-        const result = await blogCollection.updateOne({id: id}, {$set: {
+        const result = await blogCollection.updateOne({_id: new ObjectId(id)}, {$set: {
                 name: body.name,
                 description: body.description,
                 websiteUrl: body.websiteUrl,
@@ -53,17 +17,7 @@ export const blogRepository = {
         return result.matchedCount === 1;
     },
     async deleteBlogById(id: string): Promise<boolean>{
-        const result = await blogCollection.deleteOne({id: id});
+        const result = await blogCollection.deleteOne({_id: new ObjectId(id)});
         return result.deletedCount === 1;
-    },
-    mapToOutput(blog: BlogDBType): BlogViewModel {
-        return {
-            id: blog.id,
-            name: blog.name,
-            description: blog.description,
-            websiteUrl: blog.websiteUrl,
-            createdAt: blog.createdAt,
-            isMembership: blog.isMembership
-        }
     }
 }
--- src/modules/posts/post-repository.ts
+++ src/modules/posts/post-repository.ts
@@ -1,76 +1,26 @@
-import {PostDBType, PostInputModel, PostViewModel} from "../../types/post.types";
-import {blogRepository} from "../blogs/blog-repository";
+import {PostDBType, PostInputModel} from "../../types/post.types";
 import {postCollection} from "../../db/mongoDb";
+import {ObjectId} from "mongodb";
 
 
 export const postRepository = {
-    async getPosts(pageNumber: number,
-                   pageSize: number,
-                   sortBy: string,
-                   sortDirection: 'asc' | 'desc',
-                   blogId?: string): Promise<PostViewModel[]> {
-
-        const filter: any = {}
-
-        if (blogId) {
-            filter.blogId = blogId ;
-        }
-
-        const posts = await postCollection
-            .find(filter)
-            .sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
-            .skip((pageNumber - 1) * pageSize)
-            .limit(pageSize)
-            .toArray()
-
-        return posts.map(this.mapToOutput)
-    },
-    async getPostsCount(blogId?: string): Promise<number> {
-        const filter: any = {}
-
-        if (blogId) {
-            filter.blogId = blogId ;
-        }
-
-        return postCollection.countDocuments(filter)
-    },
     async createPost(post: PostDBType): Promise<string>{
-        await postCollection.insertOne(post);
-        return post.id;
+        const result = await postCollection.insertOne(post);
+        return result.insertedId.toString();
     },
-    async getPostById(id: string): Promise<PostViewModel | null> {
-        const post = await postCollection.findOne({id: id});
-        if (!post) {
-            return null;
-        }
-        return this.mapToOutput(post);
-    },
-    async updatePostById(id: string, body: PostInputModel): Promise<boolean> {
-        const blog = await blogRepository.getBlogById(body.blogId)
-
-        const result = await postCollection.updateOne({id: id}, {$set: {
+    async updatePostById(id: string, blogName: string, body: PostInputModel): Promise<boolean> {
+        const result = await postCollection.updateOne({_id: new ObjectId(id)}, {$set: {
                 title: body.title,
                 shortDescription: body.shortDescription,
                 content: body.content,
                 blogId: body.blogId,
-                blogName: blog!.name
+                blogName: blogName
             }})
 
         return result.matchedCount === 1;
     },
     async deletePostById(id: string): Promise<boolean>{
-        const result = await postCollection.deleteOne({id: id});
+        const result = await postCollection.deleteOne({_id: new ObjectId(id)});
         return result.deletedCount === 1;
-    },
-    mapToOutput(post: PostDBType): PostViewModel {
-        return {
-            id: post.id,
-            title: post.title,
-            shortDescription: post.shortDescription,
-            content: post.content,
-            blogId: post.blogId,
-            blogName: post.blogName,
-            createdAt: post.createdAt,
-        }
     }
 }
