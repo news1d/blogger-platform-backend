4c434620ae8a8a6baee6f1d0fe90b6a00a5bfb98
news1d <news1dee@yandex.ru>
2024-11-07 21:38:56 +0300

    Removed functions related to get requests.

--- src/modules/blogs/blog-service.ts
+++ src/modules/blogs/blog-service.ts
@@ -1,31 +1,12 @@
-import {BlogDBType, BlogInputModel, BlogViewModel} from "../../types/blog.types";
+import {BlogDBType, BlogInputModel} from "../../types/blog.types";
 import {blogRepository} from "./blog-repository";
 import {postRepository} from "../posts/post-repository";
-import {PostDBType, PostInputModel, PostViewModel} from "../../types/post.types";
+import {PostDBType, PostInputModel} from "../../types/post.types";
 
 
 export const blogService = {
-    async getBlogs(pageNumber: number,
-                   pageSize: number,
-                   sortBy: string,
-                   sortDirection: 'asc' | 'desc',
-                   searchNameTerm: string | null) {
-
-        const blogs = await blogRepository.getBlogs(pageNumber, pageSize, sortBy, sortDirection, searchNameTerm);
-        const blogsCount = await blogRepository.getBlogsCount(searchNameTerm)
-
-        return {
-            pagesCount: Math.ceil(blogsCount/pageSize),
-            page: pageNumber,
-            pageSize: pageSize,
-            totalCount: blogsCount,
-            items: blogs,
-        }
-    },
     async createBlog(body: BlogInputModel): Promise<string> {
         const blog: BlogDBType = {
-            _id: undefined,
-            id: Date.now().toString() + Math.floor(Math.random() * 1000000).toString(),
             name: body.name,
             description: body.description,
             websiteUrl: body.websiteUrl,
@@ -34,54 +15,22 @@ export const blogService = {
         }
         return await blogRepository.createBlog(blog)
     },
-    async getPostsByBlogId(blogId: string,
-                           pageNumber: number,
-                           pageSize: number,
-                           sortBy: string,
-                           sortDirection: 'asc' | 'desc') {
-
-        const posts = await postRepository.getPosts(pageNumber, pageSize, sortBy, sortDirection, blogId);
-        const postsCount = await postRepository.getPostsCount(blogId)
-
-        return {
-            pagesCount: Math.ceil(postsCount/pageSize),
-            page: pageNumber,
-            pageSize: pageSize,
-            totalCount: postsCount,
-            items: posts,
-        }
-
-    },
-    async createPostsByBlogId(blogId: string, body: Omit<PostInputModel, 'blogId'>): Promise<string | null> {
-        const blog = await blogRepository.getBlogById(blogId);
-
-        if (!blog) {
-            return null;
-        }
-
+    async createPostsByBlogId(blogId: string, blogName: string, body: Omit<PostInputModel, 'blogId'>): Promise<string> {
         const post: PostDBType = {
-            _id: undefined,
-            id: Date.now().toString() + Math.floor(Math.random() * 1000000).toString(),
             title: body.title,
             shortDescription: body.shortDescription,
             content: body.content,
-            blogId: blog.id,
-            blogName: blog.name,
+            blogId: blogId,
+            blogName: blogName,
             createdAt: new Date().toISOString()
         }
 
         return await postRepository.createPost(post)
-    },
-    async getPostById(id: string): Promise<PostViewModel | null> {
-        return postRepository.getPostById(id);
-    },
-    async getBlogById(id: string): Promise<BlogViewModel | null>{
-        return blogRepository.getBlogById(id)
     },
     async updateBlogById(id: string, body: BlogInputModel): Promise<boolean>{
         return await blogRepository.updateBlogById(id, body)
     },
     async deleteBlogById(id: string): Promise<boolean>{
         return await blogRepository.deleteBlogById(id)
-    },
+    }
 }
--- src/modules/posts/post-service.ts
+++ src/modules/posts/post-service.ts
@@ -1,44 +1,21 @@
-import {PostDBType, PostInputModel, PostViewModel} from "../../types/post.types";
-import {blogRepository} from "../blogs/blog-repository";
+import {PostDBType, PostInputModel} from "../../types/post.types";
 import {postRepository} from "./post-repository";
 
 
 export const postService = {
-    async getPosts(pageNumber: number,
-                   pageSize: number,
-                   sortBy: string,
-                   sortDirection: 'asc' | 'desc') {
-
-        const posts = await postRepository.getPosts(pageNumber, pageSize, sortBy, sortDirection);
-        const postsCount = await postRepository.getPostsCount()
-
-        return {
-            pagesCount: Math.ceil(postsCount/pageSize),
-            page: pageNumber,
-            pageSize: pageSize,
-            totalCount: postsCount,
-            items: posts,
-        }
-    },
-    async createPost(body: PostInputModel): Promise<string>{
-        const blog = await blogRepository.getBlogById(body.blogId)
+    async createPost(blogName: string, body: PostInputModel): Promise<string>{
         const post: PostDBType = {
-            _id: undefined,
-            id: Date.now().toString() + Math.floor(Math.random() * 1000000).toString(),
             title: body.title,
             shortDescription: body.shortDescription,
             content: body.content,
             blogId: body.blogId,
-            blogName: blog!.name,
+            blogName: blogName,
             createdAt: new Date().toISOString()
         }
         return await postRepository.createPost(post);
     },
-    async getPostById(id: string): Promise<PostViewModel | null> {
-        return postRepository.getPostById(id);
-    },
-    async updatePostById(id: string, body: PostInputModel): Promise<boolean> {
-        return await postRepository.updatePostById(id, body);
+    async updatePostById(id: string, blogName: string, body: PostInputModel): Promise<boolean> {
+        return await postRepository.updatePostById(id, blogName, body);
     },
     async deletePostById(id: string): Promise<boolean>{
         return await postRepository.deletePostById(id)
