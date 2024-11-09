3b934e8aade12b55344b49f91da7ade95c9d18be
news1d <news1dee@yandex.ru>
2024-11-09 23:01:57 +0300

    Added users and auth endpoints

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,19 @@ import {SETTINGS} from "./settings";
 import {blogRouter} from "./modules/blogs/blog-router";
 import {postRouter} from "./modules/posts/post-router";
 import {testingRouter} from "./modules/testing/testing-router";
+import {authRouter} from "./modules/auth/auth-router";
+import {userRouter} from "./modules/users/user-router";
 
 export const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(200).json({version: '1.0'})
+    res.status(200).json({version: '2.1'})
 })
 
 app.use(SETTINGS.PATH.BLOGS, blogRouter)
 app.use(SETTINGS.PATH.POSTS, postRouter);
 app.use(SETTINGS.PATH.TESTING, testingRouter);
+app.use(SETTINGS.PATH.USERS, userRouter);
+app.use(SETTINGS.PATH.AUTH, authRouter);
