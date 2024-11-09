467a9248c399e5a5dbd28b7a4aa80adc462ec2d5
news1d <news1dee@yandex.ru>
2024-11-10 01:57:37 +0300

    Added clearing of users collection

--- src/modules/testing/testing-repository.ts
+++ src/modules/testing/testing-repository.ts
@@ -1,8 +1,9 @@
-import {blogCollection, postCollection} from "../../db/mongoDb";
+import {blogCollection, postCollection, userCollection} from "../../db/mongoDb";
 
 export const testingRepository = {
     async clearDB() {
         await blogCollection.deleteMany({});
         await postCollection.deleteMany({});
+        await userCollection.deleteMany({});
     }
 }
