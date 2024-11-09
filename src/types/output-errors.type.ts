570373067d741579cdc403ecc245bc27e49fc66c
news1d <news1dee@yandex.ru>
2024-11-09 23:03:47 +0300

    Added UserInputModel type for FieldNamesType

--- src/types/output-errors.type.ts
+++ src/types/output-errors.type.ts
@@ -1,8 +1,10 @@
 import {BlogInputModel} from "./blog.types";
 import {PostInputModel} from "./post.types";
+import {UserInputModel} from "./user.types";
 
 
-export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel
+export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel | keyof UserInputModel
+
 
 export type OutputErrorsType  = {
     errorsMessages:{
