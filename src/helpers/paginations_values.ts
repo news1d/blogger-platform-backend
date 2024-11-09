ebde74fd2a4e57dbaf1bf43a93f6fb98178c9c80
news1d <news1dee@yandex.ru>
2024-11-09 22:56:33 +0300

    Added query pagination helper for users endpoint

--- src/helpers/paginations_values.ts
+++ src/helpers/paginations_values.ts
@@ -10,3 +10,14 @@ export const paginationQueries = (req: Request) => {
 
     return {pageNumber, pageSize, sortBy, sortDirection, searchNameTerm};
 }
+
+export const userPaginationQueries = (req: Request) => {
+    let pageNumber: number = req.query.pageNumber ? +req.query.pageNumber : 1;
+    let pageSize: number = req.query.pageSize ? +req.query.pageSize : 10;
+    let sortBy: string = req.query.sortBy ? req.query.sortBy.toString() : 'createdAt';
+    let sortDirection: SortDirection = req.query.sortDirection && req.query.sortDirection.toString() === 'asc' ? 'asc' : 'desc';
+    let searchLoginTerm: string | null = req.query.searchLoginTerm ? req.query.searchLoginTerm.toString() : null;
+    let searchEmailTerm: string | null = req.query.searchEmailTerm ? req.query.searchEmailTerm.toString() : null;
+
+    return {pageNumber, pageSize, sortBy, sortDirection, searchLoginTerm, searchEmailTerm};
+}
