dba9446d354804a6c457874bd8e3a05f9161f82c
news1d <news1dee@yandex.ru>
2024-11-17 22:30:44 +0300

    Added new endpoint

--- src/modules/auth/auth-router.ts
+++ src/modules/auth/auth-router.ts
@@ -2,6 +2,7 @@ import {Router} from "express";
 import {authController} from "./auth-controller";
 import {userloginOrEmailValidator, userPasswordValidator} from "../../validation/express-validator/field-validators";
 import {errorsResultMiddleware} from "../../validation/express-validator/errors-result-middleware";
+import {accessTokenMiddleware} from "../../middlewares/authorization-middleware";
 
 
 export const authRouter = Router();
@@ -10,3 +11,5 @@ authRouter.post('/login', userloginOrEmailValidator,
     userPasswordValidator,
     errorsResultMiddleware,
     authController.authentication)
+authRouter.get('/me', accessTokenMiddleware,
+    authController.getMyInfo)
