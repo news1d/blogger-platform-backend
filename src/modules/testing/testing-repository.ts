49b18975432597e38eda565cbc25c3edddb78aa3
news1d <news1dee@yandex.ru>
2024-11-17 22:22:25 +0300

    Added comment collection

--- src/modules/testing/testing-repository.ts
+++ src/modules/testing/testing-repository.ts
@@ -1,9 +1,10 @@
-import {blogCollection, postCollection, userCollection} from "../../db/mongoDb";
+import {blogCollection, commentCollection, postCollection, userCollection} from "../../db/mongoDb";
 
 export const testingRepository = {
     async clearDB() {
         await blogCollection.deleteMany({});
         await postCollection.deleteMany({});
         await userCollection.deleteMany({});
+        await commentCollection.deleteMany({});
     }
 }
