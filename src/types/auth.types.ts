f598e4d7faa01c669682b6bdac20c14574a9c459
news1d <news1dee@yandex.ru>
2024-11-17 22:29:38 +0300

    Added new view model types

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -2,3 +2,13 @@ export type LoginInputModel = {
     loginOrEmail: string;
     password: string;
 }
+
+export type LoginSuccessViewModel = {
+    accessToken: string;
+}
+
+export type MeViewModel = {
+    email: string;
+    login: string;
+    userId: string;
+}
