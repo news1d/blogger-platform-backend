576343d801b3e8cea98df981dea65ff5b85e0145
news1d <news1dee@yandex.ru>
2024-11-17 22:27:55 +0300

    Added accessTokenMiddleware

--- src/middlewares/authorization-middleware.ts
+++ src/middlewares/authorization-middleware.ts
@@ -1,12 +1,11 @@
 import {SETTINGS} from "../settings";
 import {NextFunction, Request, Response} from "express";
 import {HTTP_STATUSES} from "../helpers/http-statuses";
-
+import {jwtService} from "../application/jwt-service";
 
 export const ADMIN_AUTH = SETTINGS.ADMIN_AUTH;
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const encodedAuthData = Buffer.from(ADMIN_AUTH).toString('base64');
     const validAuthValue = `Basic ${encodedAuthData}`;
     const authHeader = req.headers.authorization?.trim();
@@ -15,5 +14,22 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
         next();
     } else {
         res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
+    }
+}
+
+export const accessTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization) {
+        res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
+        return;
+    }
+
+    const token = req.headers.authorization.split(' ')[1]
+
+    const userId = await jwtService.getUserIdByToken(token)
+    if (userId) {
+        req.userId = userId
+        next();
+    } else {
+        res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
     }
 }
