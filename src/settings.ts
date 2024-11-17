20a52a585d848ff7fe5cf6af60653ac3cff749af
news1d <news1dee@yandex.ru>
2024-11-17 22:20:50 +0300

    Added secret key for jwt, path and collection name for comments

--- src/settings.ts
+++ src/settings.ts
@@ -9,13 +9,16 @@ export const SETTINGS = {
         POSTS: '/api/posts',
         TESTING: '/api/testing',
         USERS: '/api/users',
-        AUTH: '/api/auth'
+        AUTH: '/api/auth',
+        COMMENTS: '/api/comments',
     },
     COLLECTION_NAME: {
         BLOG: 'blogs',
         POST: 'posts',
         USER: 'users',
+        COMMENT: 'comments'
     },
     MONGO_URL: process.env.MONGO_URL || 'mongodb://0.0.0.0:27017',
-    DB_NAME: process.env.DB_NAME || ''
+    DB_NAME: process.env.DB_NAME || '',
+    JWT_SECRET: process.env.JWT_SECRET || '123'
 }
