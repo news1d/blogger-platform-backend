7c0729392012d447d10474b6672a2443d31b08e4
news1d <news1dee@yandex.ru>
2024-11-18 15:39:39 +0300

    Added logging for debug

--- src/modules/auth/auth-controller.ts
+++ src/modules/auth/auth-controller.ts
@@ -3,10 +3,21 @@ import {userService} from "../users/user-service";
 import {HTTP_STATUSES} from "../../helpers/http-statuses";
 import {LoginInputModel, LoginSuccessViewModel, MeViewModel} from "../../types/auth.types";
 import {jwtService} from "../../application/jwt-service";
+import {userQueryRepo} from "../users/user-queryRepo";
+import {userPaginationQueries} from "../../helpers/paginations_values";
 
 export const authController = {
     async authentication (req: Request<any, any, LoginInputModel>, res: Response<LoginSuccessViewModel>){
+
+        console.log('loginOrEmail: ', req.body.loginOrEmail, '\npassword: ', req.body.password)
+        // console.log('password: ', req.body.password)
+        const {pageNumber, pageSize, sortBy, sortDirection, searchLoginTerm, searchEmailTerm} = userPaginationQueries(req)
+        const users = await userQueryRepo.getUsers(pageNumber, pageSize, sortBy, sortDirection, searchLoginTerm, searchEmailTerm)
+        console.log('users: ', users)
+
         const result = await userService.checkCredentials(req.body.loginOrEmail, req.body.password)
+
+        console.log('userId: ', result)
         if (!result) {
             res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
             return;
