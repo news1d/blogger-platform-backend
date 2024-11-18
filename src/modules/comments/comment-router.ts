19f7f8fa93b58aa12033d222e6a760751af6b93e
news1d <news1dee@yandex.ru>
2024-11-18 17:23:15 +0300

    Added middleware for error collection

--- src/modules/comments/comment-router.ts
+++ src/modules/comments/comment-router.ts
@@ -2,6 +2,7 @@ import {Router} from "express";
 import {accessTokenMiddleware} from "../../middlewares/authorization-middleware";
 import {commentController} from "./comment-controller";
 import {commentContentValidator} from "../../validation/express-validator/field-validators";
+import {errorsResultMiddleware} from "../../validation/express-validator/errors-result-middleware";
 
 
 export const commentRouter = Router()
@@ -9,6 +10,7 @@ export const commentRouter = Router()
 commentRouter.get('/:id', commentController.getCommentById)
 commentRouter.put('/:commentId', accessTokenMiddleware,
     commentContentValidator,
+    errorsResultMiddleware,
     commentController.updateCommentById)
 commentRouter.delete('/:commentId', accessTokenMiddleware,
     commentController.deleteCommentById);
