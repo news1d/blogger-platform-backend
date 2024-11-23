2198bc6064c49888754cf264db86c19a4ddabdfd
news1d <news1dee@yandex.ru>
2024-11-23 17:19:31 +0300

    Small changes

--- src/types/output-errors.type.ts
+++ src/types/output-errors.type.ts
@@ -1,9 +1,10 @@
 import {BlogInputModel} from "./blog.types";
 import {PostInputModel} from "./post.types";
 import {UserInputModel} from "./user.types";
+import {RegisterConfCodeModel} from "./registration.types";
 
 
-export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel | keyof UserInputModel
+export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel | keyof UserInputModel | keyof RegisterConfCodeModel
 
 
 export type OutputErrorsType  = {
