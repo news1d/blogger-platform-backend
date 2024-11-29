a539baa30e0ec893eebfee903203ac8723e8a69d
news1d <news1dee@yandex.ru>
2024-11-29 15:02:01 +0300

    Added new handler for creating refresh token

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -4,11 +4,14 @@ import {SETTINGS} from "../settings";
 
 export const jwtService = {
     async createJWT(userId: string): Promise<string>{
-        return jwt.sign({userId: new ObjectId(userId)}, SETTINGS.JWT_SECRET, {expiresIn: "1h"})
+        return jwt.sign({userId: new ObjectId(userId), unique: Date.now()}, SETTINGS.JWT_SECRET, {expiresIn: "10s"})
     },
-    async getUserIdByToken(token: string) {
+    async createRT(userId: string): Promise<string>{
+        return jwt.sign({userId: new ObjectId(userId), unique: Date.now()}, SETTINGS.REFRESH_SECRET, {expiresIn: "20s"})
+    },
+    async getUserIdByToken(token: string, secret_phrase: string) {
         try {
-            const result: any = jwt.verify(token, SETTINGS.JWT_SECRET)
+            const result: any = jwt.verify(token, secret_phrase)
             return result.userId.toString()
         } catch (error) {
             return null
