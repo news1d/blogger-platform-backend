7b5f5f0315a0d17661755cf9ee048a515be5e3ef
news1d <news1dee@yandex.ru>
2024-12-11 22:03:22 +0300

    Added rateLimitMiddleware

--- src/modules/auth/auth-router.ts
+++ src/modules/auth/auth-router.ts
@@ -8,6 +8,7 @@ import {
 } from "../../validation/express-validator/field-validators";
 import {errorsResultMiddleware} from "../../validation/express-validator/errors-result-middleware";
 import {accessTokenMiddleware, refreshTokenMiddleware} from "../../middlewares/authorization-middleware";
+import {rateLimitMiddleware} from "../../middlewares/rate-limit-middleware";
 
 
 export const authRouter = Router();
@@ -15,19 +16,23 @@ export const authRouter = Router();
 authRouter.post('/login', userloginOrEmailValidator,
     userPasswordValidator,
     errorsResultMiddleware,
+    rateLimitMiddleware,
     authController.login)
 authRouter.post('/refresh-token', refreshTokenMiddleware,
     authController.refreshToken)
 authRouter.post('/registration-confirmation',
+    rateLimitMiddleware,
     authController.registerConfirmation)
 authRouter.post('/registration',
     userLoginValidator,
     userPasswordValidator,
     userEmailValidator,
     errorsResultMiddleware,
+    rateLimitMiddleware,
     authController.registration)
 authRouter.post('/registration-email-resending',
     userEmailValidator,
+    rateLimitMiddleware,
     authController.registerEmailResending)
 authRouter.post('/logout', refreshTokenMiddleware,
     authController.logout)
