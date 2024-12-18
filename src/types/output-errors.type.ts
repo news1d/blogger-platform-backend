241fc1840d96cd895e70f34f8666926df3d4bd01
news1d <news1dee@yandex.ru>
2024-12-18 22:41:23 +0300

    Added new type

--- src/types/output-errors.type.ts
+++ src/types/output-errors.type.ts
@@ -2,9 +2,10 @@ import {BlogInputModel} from "./blog.types";
 import {PostInputModel} from "./post.types";
 import {UserInputModel} from "./user.types";
 import {RegisterConfCodeModel} from "./registration.types";
+import {NewPasswordRecoveryInputModel} from "./auth.types";
 
 
-export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel | keyof UserInputModel | keyof RegisterConfCodeModel
+export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel | keyof UserInputModel | keyof RegisterConfCodeModel | keyof NewPasswordRecoveryInputModel;
 
 
 export type OutputErrorsType  = {
