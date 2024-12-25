5dbd4c0e7e071d481b0e314e1ed09a4f21da3905
news1d <news1dee@yandex.ru>
2024-12-25 15:41:44 +0300

    CommentSchema modified

--- src/entities/comment.entity.ts
+++ src/entities/comment.entity.ts
@@ -2,6 +2,13 @@ import mongoose from "mongoose";
 import {WithId} from "mongodb";
 import {CommentDBType} from "../types/comments.types";
 import {SETTINGS} from "../settings";
+import {LikesDBType, LikeStatus} from "../types/like.types";
+
+const likeSchema = new mongoose.Schema<LikesDBType>({
+    createdAt: { type: Date, required: true },
+    status: { type: String, enum: Object.values(LikeStatus), required: true },
+    authorId: { type: String, required: true },
+})
 
 export const CommentSchema = new mongoose.Schema<WithId<CommentDBType>>({
     content: { type: String, required: true },
@@ -11,6 +18,9 @@ export const CommentSchema = new mongoose.Schema<WithId<CommentDBType>>({
     },
     createdAt: { type: String, required: true },
     postId: { type: String, required: true },
+    likes: { type: [likeSchema] },
+    likesCount: { type: Number , required: true },
+    dislikesCount: { type: Number , required: true },
 })
 
 export const CommentModel = mongoose.model<WithId<CommentDBType>>(SETTINGS.COLLECTION_NAME.COMMENT, CommentSchema)
